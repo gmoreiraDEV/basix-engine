ced076daca9c2de637d91f399fb2e400c3748e3b
Guilherme Moreira <92859054+gmoreiraDEV@users.noreply.github.com>
2025-11-03 11:48:57 -0300

    Add script to fetch and save product data
    Fetch product data from an API and save to a JSON file.

---
+++ workflows/svim_brain/tasks/svim_greetings.py
@@ -0,0 +1,13 @@
+import json
+
+def main():
+  response = requests.get(
+    url='https://dummyjson.com/products'
+  )
+  products_data = response.json().get('products')
+
+  with open('products.json', 'w', enconding='utf-8') as f:
+    json.dump(products_data, f, indent=2, ensure_ascii=False)
+
+if __name__ == '__main__':
+  main()
