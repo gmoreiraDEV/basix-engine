b0ece8266fcaafbc9e1c50748b23dbf259e49a43
Guilherme Moreira <92859054+gmoreiraDEV@users.noreply.github.com>
2025-11-03 22:18:45 +0000

    Create svim router flow

---
+++ workflows/svim_router_flow/tasks/professional_task.py
@@ -0,0 +1,3 @@
+print("Buscando profissionais disponíveis...")
+pros = ["Ana", "Júlia"]
+print("Profissionais:", pros)
---
+++ workflows/svim_router_flow/tasks/service_task.py
@@ -0,0 +1,4 @@
+print("Executando lógica de seleção de serviço...")
+service = "Unha de Gel"
+print(f"Serviço escolhido: {service}")
+
