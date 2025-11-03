79f53678ed2e12b574230ae9828ca092c5520e18
Guilherme Moreira <92859054+gmoreiraDEV@users.noreply.github.com>
2025-11-03 14:25:51 -0300

    Update svim_greetings.py

--- workflows/svim_brain/tasks/svim_greetings.py
+++ workflows/svim_brain/tasks/svim_greetings.py
@@ -2,13 +2,15 @@ import json
 import requests
 
 def main():
-  response = requests.get(
-    url='https://dummyjson.com/products'
-  )
-  products_data = response.json().get('products')
+    response = requests.get("https://dummyjson.com/products")
+    response.raise_for_status()  # levanta erro se a requisição falhar
 
-  with open('products.json', 'w', enconding='utf-8') as f:
-    json.dump(products_data, f, indent=2, ensure_ascii=False)
+    products_data = response.json().get("products", [])
 
-if __name__ == '__main__':
-  main()
+    with open("products.json", "w", encoding="utf-8") as f:
+        json.dump(products_data, f, indent=2, ensure_ascii=False)
+
+    print("Arquivo 'products.json' salvo com sucesso!")
+
+if __name__ == "__main__":
+    main()
